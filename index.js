bb05014991b2a350c7e51a1d33510e63e5bef272
Ayma-n <aymanbt14@gmail.com>
2022-02-20 22:50:24 -0500

    rewrote code with readFileSync

--- index.js
+++ index.js
@@ -8,26 +8,20 @@ const decryptedString = CryptoJS.AES.decrypt(encryptedString, "super-secret-key"
 console.log(decryptedString.toString(CryptoJS.enc.Utf8));
 
 const encrypt = () => {
-    fs.readFile('plain-imgs/2560px-Npm-logo.svg.png', (err, data) => {
-        if (err) throw err;
-        const imgBase64 = data.toString('base64');
-        const encryptedImg = CryptoJS.AES.encrypt(imgBase64, "super-secret-key");
-        const buffer = Buffer.from(encryptedImg.toString());
-
-        fs.writeFile("encrypted-imgs/file.encrypted", buffer, (err) => {
-            if (err) throw err;
-        });
-    })
+    const originalFileBuffer = fs.readFileSync("plain-imgs/2560px-Npm-logo.svg.png");
+    const imgBase64 = originalFileBuffer.toString('base64');
+    const encryptedImg = CryptoJS.AES.encrypt(imgBase64, "super-secret-key");
+    const buffer = Buffer.from(encryptedImg.toString());
+    fs.writeFileSync("encrypted-imgs/file.encrypted", buffer);
 }
 
 const decrypt = () => {
-    fs.readFile("encrypted-imgs/file.encrypted", (err, data) => {
-        if (err) throw err;
-        const encryptedImgString = data.toString();
-        const decryptedFile = CryptoJS.AES.decrypt(encryptedImgString, "super-secret-key");
-        const decryptedFileString = decryptedFile.toString(CryptoJS.enc.Utf8);
-        fs.writeFile("plain-imgs/decrypted.png", decryptedFileString, 'base64', (err) => {
-            if (err) throw err;
-        })
-    })
+    const encryptedFileBuffer = fs.readFileSync("encrypted-imgs/file.encrypted");
+    const encryptedImgString = encryptedFileBuffer.toString();
+    const decryptedFile = CryptoJS.AES.decrypt(encryptedImgString, "super-secret-key");
+    const decryptedFileString = decryptedFile.toString(CryptoJS.enc.Utf8);
+    fs.writeFileSync("plain-imgs/decrypted.png", decryptedFileString, 'base64');
 }
+
+encrypt();
+decrypt();
